658d2d4616808a3ceb37586f14e05b7ced2567c6
vanyog <info@vanyog.com>
2021-01-27 17:00:10 +0200

    Calculator project

---
+++ Calculator/MainWindow.vb
@@ -0,0 +1,40 @@
+﻿Public Class MainWindow
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Double
+        Double.TryParse(TextBox1.Text, a)
+        Dim b As Double
+        Double.TryParse(TextBox2.Text, b)
+        Dim c As Double = a + b
+        TextBox3.Text = Str(c)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim a As Double
+        Double.TryParse(TextBox1.Text, a)
+        Dim b As Double
+        Double.TryParse(TextBox2.Text, b)
+        Dim c As Double = a - b
+        TextBox3.Text = Str(c)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim a As Double
+        Double.TryParse(TextBox1.Text, a)
+        Dim b As Double
+        Double.TryParse(TextBox2.Text, b)
+        Dim c As Double = a * b
+        TextBox3.Text = Str(c)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim a As Double
+        Double.TryParse(TextBox1.Text, a)
+        Dim b As Double
+        Double.TryParse(TextBox2.Text, b)
+        Dim c As Double = a / b
+        TextBox3.Text = Str(c)
+    End Sub
+
+End Class
+
